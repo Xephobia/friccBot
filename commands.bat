d1bf5605089c7baee41e8b544e67e21373446852
Xephobia <nathanvalletmarseille@gmail.com>
2020-07-06 22:06:41 +0200

    move commands

--- commands.bat
+++
@@ -1,8 +0,0 @@
-haxelib setup ../
-haxelib install hxcpp
-haxelib install hxcs
-haxelib git haxicord https://github.com/RaidAndFade/Haxicord.git
-haxe cpp.hxml
-haxe js.hxml
-haxe csharp.hxml
-haxe neko.hxml
